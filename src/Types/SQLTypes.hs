f57381d8c8640e4a01779ead1210515626031780
Jason Hom <61599018+homjason@users.noreply.github.com>
2022-11-28 14:55:08 -0500

    :zap: Working Session 2022-11-28

--- src/Types/SQLTypes.hs
+++ src/Types/SQLTypes.hs
@@ -13,32 +13,52 @@ import Test.QuickCheck
 newtype Query = Query [Statement]
   deriving (Eq, Show)
 
+-- QUESTION: Do we need semigroup/monoid?
 -- instance Semigroup SQLQuery where
 --   SQLQuery s1 <> SQLQuery s2 = Block (s1 <> s2)
 
 -- instance Monoid SQLQuery where
 --   mempty = SQLQuery []
 
+-- TODO: JOIN, HAVING, Set Operations
+
 data Statement
-  = Select Table Var -- x = e
-  -- = Select [Col] Query
-  | Empty -- ';'
+  = Select SelectExp -- x = e
+  | From FromExp
+  | Where WhereExp
+  | GroupBy [Name] -- Group by one or more columns
+  | Limit Int -- Limit no. of rows in query output
+  | OrderBy Name Order -- Can only order by one column
+  | Empty -- ; (semicolon signalling the end of a SQL query)
   deriving (Eq, Show)
 
--- stuff that comes after the Select clause
+-- Order in which to sort query results (ascending or descending)
+data Order = Asc | Desc
+  deriving (Eq, Show)
+
+-- Datatype for SELECT clauses in SQL
 data SelectExp
-  = ColNames [Name] -- colnames are a list of string names
+  = Cols [Name] -- colnames are a list of string names
+  | DistinctCols [Name] -- SELECT DISTINCT in SQL
+  | Agg AggFunc Name -- Aggregate functions (used with GROUP BY clauses)
   | Val Value -- literal values
-  | Alias Name -- equivalent to "AS" keyword in SQL
+  | As Name -- "AS" keyword in SQL (renaming columns)
   deriving (Eq, Show)
 
+-- Datatype for FROM clauses in SQL
+-- We can either select from a named table or from a subquery
 data FromExp
   = TableName Name
   | SubQuery Query
   deriving (Eq, Show)
 
+-- Datatype for WHERE clauses in SQL
 data WhereExp
-  = Op2 Comparable Comparable
+  = OpC Comparable Comparable -- Comparison operations
+  | OpA Comparable Comparable -- Arithmetic Operations
+  | OpL Bool Bool -- Logical operations
+  | OpN Name -- NULL/IS NULL
+  deriving (Eq, Show)
 
 data Value
   = NilVal -- nil
@@ -48,51 +68,50 @@ data Value
   | TableVal Name -- <not used in source programs>
   deriving (Eq, Show, Ord)
 
-data Uop
-  = Neg -- `-` :: Int -> Int
-  | Not -- `not` :: a -> Bool
-  | Len -- `#` :: String -> Int / Table -> Int
-  deriving (Eq, Show, Enum, Bounded)
-
 -- Type representing values that can be compared in SQL queries
 data Comparable
   = ColName Name -- column name
   | Int
+  deriving (Eq, Show)
 
--- Boolean operations
-data BoolBop
-  = Eq -- `==` :: Comparable -> Comparable -> Bool
+-- Comparison (binary) operations that return a Boolean
+data CompOp
+  = Eq -- `=` :: Comparable -> Comparable -> Bool
   | Gt -- `>`  :: Comparable -> Comparable -> Bool
   | Ge -- `>=` :: Comparable -> Comparable -> Bool
   | Lt -- `<`  :: Comparable -> Comparable -> Bool
   | Le -- `<=` :: Comparable -> Comparable -> Bool
   deriving (Eq, Show, Enum, Bounded)
 
--- Arithmetic operations
-data ArithBop
+-- Arithmetic (binary) operations
+data ArithOp
   = Plus -- `+`  :: Comparable -> Comparable -> Comparable
   | Minus -- `-`  :: Comparable -> Comparable -> Comparable
   | Times -- `*`  :: Comparable -> Comparable -> Comparable
-  | Divide -- `//` :: Comparable -> Comparable -> Comparable   -- floor division
+  | Divide -- `/` :: Comparable -> Comparable -> Comparable   -- floor division
   | Modulo -- `%`  :: Comparable -> Comparable -> Comparable   -- modulo
+  deriving (Eq, Show, Enum, Bounded)
 
-data Sop
-  = In
-
--- (ColA `Gt` ColB) `And` (ColC `Gt` ColD)
--- And (ColA `Gt` ColB) (ColC `Gt` ColD)
-
--- Logical operators
-data Lop
+-- QUESTION: Why don't Enum or Bounded work?
+-- Logical (binary) operations
+data LogOp
   = And Bool Bool
   | Or Bool Bool
-  | IsNull Name -- Check whether a column contains null values
+  deriving (Eq, Show)
+
+-- Unary operations for checking if a column is null / not-null
+data NullOp
+  = IsNull Name -- Check whether a column contains null values
   | IsNotNull Name -- Check whether a column contains non-null values
+  deriving (Eq, Show)
+
+-- Aggregate Functions
+data AggFunc
+  = Count
+  | Avg
+  | Sum
+  | Min
+  | Max
+  deriving (Eq, Show, Enum, Bounded)
 
 type Name = String -- column names
-
-data Var
-  = Name Name -- x, global variable
-  | Dot Expression Name -- t.x, access table using string key
-  | Proj Expression Expression -- t[1], access table table using any type of key
-  deriving (Eq, Show)
