48d910ee31c0bb41c73d99b33165552f33f2e154
Jason Hom <61599018+homjason@users.noreply.github.com>
2022-12-19 09:28:27 -0500

    edit cli

--- app/Main.hs
+++ app/Main.hs
@@ -10,21 +10,7 @@ import Translator
 
 main :: IO ()
 main = do
-  putStrLn "Enter a SQL query you would like to translate to Pandas! Note the following requirements..."
+  putStrLn "Enter a SQL query you would like to translate to Pandas!"
   str <- getLine
   putStrLn "\n Pandas Result: "
   putStrLn $ runSqlToPandas str
-
--- case parseQuery str of
---   Left error -> putStrLn error
---   Right q' -> do
---     case validateQuery q' of
---       Left err -> putStrLn err
---       Right boo -> do
---         putStrLn "\nPandas Result:"
---         putStrLn $ pretty (translateSQL q')
---         return ()
-
--- unless (Right $ validateQuery q') (putStrLn "Query is malformed!")
-
--- when (not (validateQuery q')) (putStrLn "Query is malformed!")
